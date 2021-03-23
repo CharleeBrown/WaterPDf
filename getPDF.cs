ac1e9cb4724db89a0e5b26f93ece76c0aad4c95e
CharlesLee-23 <Coleman.hallom@Spray.com>
2021-03-23 15:32:39 -0500

    Added comments throughout.

--- getPDF.cs
+++ getPDF.cs
@@ -12,6 +12,7 @@ using PdfSharp.Charting;
 using PdfSharp.SharpZipLib;
 using PdfSharp.Pdf.IO;
 using System.Windows.Forms;
+using System.IO;
 
 
 namespace WatermarkAdd
@@ -24,46 +25,52 @@ namespace WatermarkAdd
 
         public void markPDF(string filepath, string watermark, string userfile, int fontSize, string destination)
         {
-
+             string userWatermark = watermark;      
             
-            
-            
-           // string defaultWatermark = "Extracted, with permission, from the Annual Book of ASTM Standards, copyright ASTM Internatinal, 100 Barr Harbor Drive, West Conshohocken, PA 19428";
-            string userWatermark = watermark;
-
-            
-
-
-            
+            // If the watermark field is empty. 
             if (string.IsNullOrEmpty(userWatermark))
             {
-                MessageBox.Show("Enter text for the watermark.", "WaterPDF"); 
+                MessageBox.Show("Enter text for the watermark.", "WaterPDF");  // Alert the user. 
+
             }
+            // If the path field is empty. 
             else if (string.IsNullOrEmpty(filepath))
             {
-                MessageBox.Show("Select a file.", "WaterPDF");
+                MessageBox.Show("Select a file.", "WaterPDF");// Alert the user
             }
+            // If the new filename is empty. 
             else if (string.IsNullOrWhiteSpace(userfile))
             {
-                MessageBox.Show("Enter a filename", "WaterPDF");
+                MessageBox.Show("Enter a filename", "WaterPDF"); // Alert the user. 
             }
             else if (string.IsNullOrWhiteSpace(destination))
             {
-                MessageBox.Show("Select a save destination");
+                MessageBox.Show("Select a save destination"); // Alert the user. 
             }
             else
             {
+                // Creates a new file path to save the watermarked PDF.
                 string newFileName = destination + @"\" + userfile + ".pdf";
+                
+
+                // Opens the file from the path provided.
                 PdfDocument document = PdfReader.Open(filepath);
 
+                // Counts the amount of pages in the PDF. 
                 int pages = document.Pages.Count;
+
+                // Sets the first page as the page to work on. 
                 PdfPage page = document.Pages[0];
+
+                // Creating the font variable. 
                 XFont font = new XFont("Arial", fontSize, XFontStyle.Regular);
 
+                // Creating the graphics. 
                 XGraphics gfx = XGraphics.FromPdfPage(page, XGraphicsPdfPageOptions.Append);
 
                 var size = gfx.MeasureString(watermark, font);
 
+                // Creating the coordinates. 
                 gfx.TranslateTransform(page.Width / 2, page.Height / 2);
                 gfx.RotateTransform(-Math.Atan(page.Height / page.Width) * 180 / Math.PI);
                 gfx.TranslateTransform(-page.Width / 2, -page.Height / 2);
@@ -72,30 +79,24 @@ namespace WatermarkAdd
                 format.Alignment = XStringAlignment.Near;
                 format.LineAlignment = XLineAlignment.Near;
 
+                // Creating the brush to paint with. 
                 XBrush brush = new XSolidBrush(XColor.FromKnownColor(XKnownColor.LightGray));
 
                 gfx.DrawString(watermark, font, brush,
                  new XPoint((page.Width - size.Width) / 2, (page.Height - size.Height) / 2),
                  format);
 
-                //string filename = @"C:\Users\hallocol\Desktop\test2.pdf";
-                
-
                 gfx.DrawString(watermark, font, brush,
                     new XPoint((page.Width - size.Width) / 2, (page.Height - size.Height) / 2),
                     format);
+
+                // Saves it in the provided location. 
                 document.Save(newFileName);
-              
-                 
                 if (MessageBox.Show("Your PDF has been watered.", "WaterPDF") == DialogResult.OK)
                 {
-                    System.Diagnostics.Process.Start("explorer.exe", destination);
-                    
+                    // Alerts the user that the file was saved and creates an explorer window of the destination.
+                    System.Diagnostics.Process.Start("explorer.exe", destination);   
                 }
-                    
-                   
-                
-               
             }
            
         }
--- pullPath.cs
+++ pullPath.cs
@@ -14,32 +14,38 @@ namespace WatermarkAdd
         string mainPath;
         public string GetFile()
         {
+            // Opens the file dialog that only looks for PDFs. 
             OpenFileDialog files = new OpenFileDialog();
             files.Filter = "PDF|*.PDF";
             files.Title = "Choose a file to import";
 
+            // If the file is ok. 
             if (files.ShowDialog() == DialogResult.OK)
             {
+                // Pull the filename and put it into the textbox. 
                 FileInfo infos = new FileInfo(files.FileName);
 
+                // Set the variable to the file name. 
                 mainPath = infos.FullName;
             }
 
-
+            // Return the filename variable. 
             return mainPath;
-
         }
 
         public string SaveFile()
         {
+            // Opens the folder browswer dialog. 
             FolderBrowserDialog  folder = new FolderBrowserDialog();
+
+            // If the folder is ok. 
             if(folder.ShowDialog() == DialogResult.OK)
             {
+                // Set the variable to the folder path. 
                  savePath = folder.SelectedPath;
             }
+            // return the folder path variable. 
             return savePath;
-
-
         }
     }
 }
